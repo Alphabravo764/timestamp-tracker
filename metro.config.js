1a97377b296eb07fef14244e456604edd5eecb1f
Areej Safdar <areejsafdar@Areejs-MacBook-Air.local>
2026-01-07 12:39:03 +0000

    Fix metro.config.js - use simplest default config

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,7 @@
-const { getDefaultConfig } = require("expo/metro-config");
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// Add CSS support for NativeWind
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve("nativewind/metro/transformer"),
-};
 
 module.exports = config;
